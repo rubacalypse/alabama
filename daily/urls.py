593bb6de43da26cc53a553945e772d573c4d0a6e
Ruba Merza <ruba.merza@gmail.com>
2016-09-20 15:41:26 -0700

    Add new project form

--- daily/urls.py
+++ daily/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
         url(r'^$', views.index, name='index'),
         url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$',
           views.show_schedule, name='show_schedule'),
+        url(r'^new_project', views.new_project, name='new'),
         ]
--- daily/views.py
+++ daily/views.py
@@ -1,17 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.utils import timezone
+from django.utils.dateparse import parse_time
 
-from .models import Project
+from .models import Project, Employee, Phone
 
 import datetime
 
 def index(request):
   today = timezone.now().date()
   todays_schedule = Project.objects.filter(date=today)
-  context = {'schedule': todays_schedule, 'date': today}
+  employee_names = Employee.objects.all()
+  context = {'schedule': todays_schedule, 'date': today, 'emp_names':
+      employee_names}
   return render(request, 'daily/div-test.html/', context)
 
 def show_schedule(request, year, month, day):
   date = datetime.date(int(year), int(month), int(day))
   return render(request, 'daily/boot-test.html', {'date': date})
+
+def new_project(request):
+  print(request.POST)
+  pname = request.POST['proj-name']
+  ptime = request.POST['proj-time']
+  pemps = request.POST['proj-emps']
+  pphones = request.POST['proj-phone']
+  print(pemps[0])
+  
+  p = Project()
+  p.name=pname
+  p.date=timezone.now().date()
+  p.time=parse_time(ptime)
+  p.save()
+  p.employee.add(Employee.objects.get(name=pemps))
+  p.phone.add(Phone.objects.get(number=pphones))
+  
+  p.save()
+  
+  #add new project
+  return HttpResponseRedirect('/daily')
