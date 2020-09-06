c1708f4c1d2c90c4ab560ee15da131c755b61300
Ruba Merza <ruba.merza@gmail.com>
2020-09-05 22:25:13 -0700

    Fix settings.py.

--- liverpool/settings.py
+++ liverpool/settings.py
@@ -151,4 +151,4 @@ LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
