c3cc7945bb6e3904075de2325ca3dba12bfa7969
Ruba Merza <ruba.merza@gmail.com>
2020-09-05 21:02:44 -0700

    Try to fix Whitenoise problems.

--- settings.py
+++ settings.py
@@ -1,1 +1,9 @@
+import django_heroku
+
 ALLOWED_HOSTS = [".herokuapp.com"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MIDDLEWARE = [
+  'django.middleware.security.SecurityMiddleware',
+  'whitenoise.middleware.WhiteNoiseMiddleware',
+  # ...
+]
