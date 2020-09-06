11747f6b3b506b01f3b4ed8e996794d0d3a786ad
Ruba Merza <ruba.merza@gmail.com>
2020-09-06 10:50:30 -0700

    Change urls.py

--- liverpool/urls.py
+++ liverpool/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import include, url, path
+from django.urls import include, path
 #from django.conf.urls import include, url
 from django.contrib import admin
 import datetime
+import daily
 
 urlpatterns = [
     path('', include('daily.urls')),
     #should probably redirect this?
-    path('^daily/', include(('daily.urls'), namespace='daily')),
-    url(r'^admin/', admin.site.urls),
+    path('^daily/', include(('daily.urls'), name='daily')),
+    path(r'^admin/', admin.site.urls),
 ]
