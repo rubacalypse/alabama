c171cf8b9d75c6d4b1a3cbb959c86a6cd0b95f7f
Ruba Merza <me@ruba.io>
2022-12-02 22:46:44 -0800

    Try using the contrib auth login view.

--- liverpool/urls.py
+++ liverpool/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.urls import include, path
-#from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 import datetime
 
 urlpatterns = [
     path('', include('daily.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     #should probably redirect this?
     path('^daily/', include('daily.urls')),
     path(r'^admin/', admin.site.urls),
